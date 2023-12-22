cab17bd0aaf8e60cfa7fb9bde853fd660cad0a47
Ho Nyeong <dev.honing@gmail.com>
2023-12-22 14:45:19 +0900

    팩토리 함수 예제와 주석 설명 작성
    - createPerson() 함수 설명
    - sayHello() 함수 설명

--- Factory Function/Factory-Function.js
+++ Factory Function/Factory-Function.js
@@ -6,3 +6,24 @@
 객체를 생성하고 반환하는 함수. 
 객체를 일관되게 생성하므로 코드의 재사용성이 높아지고, 가독성도 개선된다고 한다.
 */
+
+/**
+ * 
+ * 팩토리 함수 예제
+ * @param {*} name 이름
+ * @param {*} age 나이
+ * @returns 
+ */
+function createPerson(name, age) {
+  return {
+    name: name,
+    age: age,
+
+    /**
+     * 인사말을 출력하기 위한 함수
+     */
+    sayHello: function() {
+      console.log(`ㅎㅇ 내 이름은 ${this.name}이고, 나이는 ${this.age}살임.`)
+    }
+  }
+}
