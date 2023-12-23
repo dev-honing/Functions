e27294f29c81260ed7869681407ed4310b2640eb
Ho Nyeong <dev.honing@gmail.com>
2023-12-23 22:19:03 +0900

    출력된 콘솔 확인하고 주석으로 작성

--- Constructor Function/Constructor-Function.js
+++ Constructor Function/Constructor-Function.js
@@ -30,5 +30,5 @@ const person1 = new Person('John', 25);
 const person2 = new Person('Jane', 30);
 
 // 객체의 메서드 호출
-person1.sayHello();
-person2.sayHello();
+person1.sayHello(); // ㅎㅇ. 내 이름은 John이고, 나이는 25살임.
+person2.sayHello(); // ㅎㅇ. 내 이름은 Jane이고, 나이는 30살임.
